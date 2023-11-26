abb50ad278a374bdb159a425fd61cb4ed2fd1e22
Franklin Gómez <franklingo13@gmail.com>
2023-11-26 17:55:27 -0500

    Update main.c
    Se agregan comentarios

--- esp-now_RX/main/main.c
+++ esp-now_RX/main/main.c
@@ -15,12 +15,16 @@
 #define ESP_CHANNEL 1
 #define LED_PIN GPIO_NUM_2
 
+// Dirección MAC del nodo receptor
 static uint8_t peer_mac[ESP_NOW_ETH_ALEN] = {0x40, 0x22, 0xd8, 0xef, 0x21, 0xd4}; // Mac Rx
-static const char *TAG = "esp_now_init";
-// Mac Franklin = 0xc8, 0xf0, 0x9e, 0x52, 0x9e, 0x64
 
+// Etiqueta para mensajes de registro
+static const char *TAG = "esp_now_init";
+
+// Estructura para el manejo de la pantalla OLED
 SSD1306_t dev;
 
+// Inicializa la conexión WiFi del ESP32
 static esp_err_t init_wifi(void)
 {
     wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
@@ -37,12 +41,14 @@ static esp_err_t init_wifi(void)
     return ESP_OK;
 }
 
+
+// Función de retroalimentación para recibir datos a través de ESP-NOW
 void rec_cb(const esp_now_recv_info_t *esp_now_info, const uint8_t *data, int data_len)
 {
     char mac_str[18];
     sprintf(mac_str, MACSTR, MAC2STR(esp_now_info->src_addr));
     ESP_LOGI(TAG, "Message received from: %s, Message: %s", mac_str, data);
-    // Actualizar la pantalla OLED con la dirección MAC y el mensaje recibido
+    // ____________Actualizar la pantalla OLED con la dirección MAC y el mensaje recibido__________
     char display_text[64];
     //sprintf(display_text, "MAC: %s", mac_str);
     // condicional para identificar al dueño de la MAC
@@ -59,14 +65,15 @@ void rec_cb(const esp_now_recv_info_t *esp_now_info, const uint8_t *data, int da
         //ssd1306_clear_screen(&dev, false);
         ssd1306_contrast(&dev, 0xff);
         ssd1306_display_text(&dev, 4, display_text, 16, true);
-        // imprimir en dos lineas el mensaje recibido
+        // imprimir en tres lineas el mensaje recibido
         ssd1306_display_text(&dev, 5, (char *)data, 12, false);
-        ssd1306_display_text(&dev, 6, (char *)data+13, 12, false);
-        ssd1306_display_text(&dev, 7, (char *)data+25, 10, false);
+        ssd1306_display_text(&dev, 6, (char *)data + 13, 12, false);
+        ssd1306_display_text(&dev, 7, (char *)data + 25, 10, false);
     }
     
 }
 
+// Inicializa la configuración de ESP-NOW
 static esp_err_t init_esp_now(void)
 {
     esp_now_init();
@@ -76,6 +83,7 @@ static esp_err_t init_esp_now(void)
     return ESP_OK;
 }
 
+// Registra el nodo receptor en ESP-NOW
 static esp_err_t register_peer(uint8_t *peer_mac)
 {
     esp_now_peer_info_t esp_now_peer_info = {};
@@ -87,6 +95,7 @@ static esp_err_t register_peer(uint8_t *peer_mac)
     return ESP_OK;
 }
 
+// Inicializa la pantalla OLED
 void oled_task()
 {
     i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
@@ -95,6 +104,7 @@ void oled_task()
     ssd1306_contrast(&dev, 0xff);
 }
 
+// Función principal de la aplicación
 void app_main(void)
 {
     ESP_ERROR_CHECK(init_wifi());
@@ -109,6 +119,7 @@ void app_main(void)
 	ssd1306_display_text(&dev, 0, "---WSN-G8---", 16, true);
 	ssd1306_software_scroll(&dev, 1, (dev._pages-1) );
     
+    // Esperar un segundo antes de continuar
     vTaskDelay(pdMS_TO_TICKS(1000));
 
 }
